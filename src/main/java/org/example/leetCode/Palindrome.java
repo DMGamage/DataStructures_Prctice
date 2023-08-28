2d1055a9fbcc6872fc50a10d89ab066238bccda1
DMGamage <gamagemadhushan@gmail.com>
2023-08-28 17:49:05 +0530

    Palindrome

---
+++ src/main/java/org/example/leetCode/Palindrome.java
@@ -0,0 +1,23 @@
+package org.example.leetCode;
+/**
+ * Have the function Palindrome(str) take the str parameter
+ * being passed and return the string true
+ * if the parameter is a palindrome,
+ * (the string is the same forward as it is backward)
+ * otherwise return the string false.
+ * For example: "racecar" is also "racecar" backwards.
+ * Punctuation and numbers will not be part of the string.
+ */
+
+public class Palindrome {
+    private static String palindrome(String str){
+        StringBuilder strBuild = new StringBuilder();
+        String cleaned = str.replaceAll(" ", "");
+        StringBuilder reverse = strBuild.append(cleaned).reverse();
+        return cleaned.equals(reverse.toString()) && str.length()>0 ? "true" :"false";
+
+
+
+    }
+
+}
---
+++ src/main/java/org/example/leetCode/StringCounter.java
@@ -0,0 +1,7 @@
+package org.example.leetCode;
+
+public class StringCounter {
+    private static int stringCounter(String str){
+
+    }
+}
